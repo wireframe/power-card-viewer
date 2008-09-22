77cc9bbf51799a289644f4563964ec58f1755424
Ryan Sonnek <ryan@socialcast.com>
2008-09-21 22:34:09 -0500

    add power importer

---
+++ to_html.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'yaml'
+require 'active_support'
+require 'haml'
+
+powers = YAML.load(File.read('powers.yml'))
+powers.each_with_index do |p,i|
+  p[:id] = i
+end
+class_categories = powers.map {|p| p[:category]}.uniq.delete_if {|p| p =~ /(Epic|Feat|Racial)/}.sort
+other_categories = ['Epic', 'Feat', 'Racial']
+
+engine = Haml::Engine.new(File.read('powers.html.haml'))
+
+File.open('../power-card-viewer/powers.html', 'w') { |f| f << engine.render(Object.new, :powers => powers, :class_categories => class_categories, :other_categories => other_categories) }
